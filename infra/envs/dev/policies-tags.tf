ad21db6a27ce38604622dae4e2e324cee62659d6
doxajay <99846671+doxajay@users.noreply.github.com>
2026-01-19 12:40:43 -0700

    Add policy to require 'Environment' tag on resources
    Add Governance: Require Tags

---
+++ infra/envs/dev/policies-tags.tf
@@ -0,0 +1,17 @@
+data "azurerm_subscription" "current" {}
+
+data "azurerm_policy_definition" "require_tag" {
+  display_name = "Require a tag on resources"
+}
+
+resource "azurerm_policy_assignment" "require_environment_tag" {
+  name                 = "require-environment-tag"
+  scope                = data.azurerm_subscription.current.id
+  policy_definition_id = data.azurerm_policy_definition.require_tag.id
+  display_name         = "Require Environment tag on all resources"
+
+  parameters = jsonencode({
+    tagName = { value = "Environment" }
+    effect  = { value = "Deny" }
+  })
+}
