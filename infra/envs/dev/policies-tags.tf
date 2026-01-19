0c8df2a31a5a3e6a5b141bba4dc7fd67b8dd5082
doxajay <99846671+doxajay@users.noreply.github.com>
2026-01-19 12:53:51 -0700

    Update policy assignment to use subscription assignment

--- infra/envs/dev/policies-tags.tf
+++ infra/envs/dev/policies-tags.tf
@@ -1,12 +1,14 @@
-
+############################################
+# Azure Policy – Require Environment Tag
+############################################
 
 data "azurerm_policy_definition" "require_tag" {
   display_name = "Require a tag on resources"
 }
 
-resource "azurerm_policy_assignment" "require_environment_tag" {
+resource "azurerm_subscription_policy_assignment" "require_environment_tag" {
   name                 = "require-environment-tag"
-  scope                = data.azurerm_subscription.current.id
+  subscription_id      = data.azurerm_subscription.current.id
   policy_definition_id = data.azurerm_policy_definition.require_tag.id
   display_name         = "Require Environment tag on all resources"
 
